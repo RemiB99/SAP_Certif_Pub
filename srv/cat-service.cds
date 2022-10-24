bf28ff6a451097eaf1f9edf65b2831dc43113cdb
Yoan Ameloot <yoan.ameloot@etu.univ-smb.fr>
2022-10-24 09:47:56 +0000

    début implémentation auth sécurisé

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,7 +1,13 @@
 using my.application as my from '../db/csv/data-model-projet';
 
 @cds.query.limit: 100000
-service SAPService {
+service SAPService @(requires :'authenticated-user') {
+  entity Users @readonly as projection on my.Users;
+  entity Questions @readonly as projection on my.Questions;
+  entity QuestionTypes @readonly as projection on my.QuestionTypes;
+}
+
+service AdminSAPService @(requires :'admin'){
   entity Users as projection on my.Users;
   entity Questions as projection on my.Questions;
   entity QuestionTypes as projection on my.QuestionTypes;
---
+++ srv/server.js
@@ -0,0 +1,20 @@
+const cds = require("@sap/cds");
+const { auth, requiresAuth } = require("express-openid-connect");
+module.exports = cds.server;
+
+const config = {
+  authRequired: false, // deactivate auth for all routes
+  auth0Logout: true, // logout from IdP
+  authorizationParams: { // required to retrieve JWT including permissions (our roles) 
+    response_type: "code",
+    scope: "openid",
+    audience: "<https://cap-auth0-SAPcertif-api.com>",
+  },
+};
+
+cds.on("bootstrap", (app) => {
+  // initialize openid-connect with auth0 configuration
+  app.use(auth(config));
+});
+
+module.exports = cds.server;
