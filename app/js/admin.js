749e497f16fb2ceb1e4758baf86a9ef5da6bf6d4
Yoan Ameloot <yoan.ameloot@etu.univ-smb.fr>
2022-11-03 16:42:19 +0000

    debut import csv

--- app/js/admin.js
+++ app/js/admin.js
@@ -485,7 +485,18 @@ function delQuestionTypButton(){
 }
 
 function addCSVButton(){
-    console.log("Ajout de questions par CSV... TODO");
+    console.log("CSV !");
+    const fileInput = document.getElementById('csv')
+    const readFile = () => {
+      const reader = new FileReader()
+      reader.onload = () => {
+        document.getElementById('out').innerHTML = reader.result
+      }
+      // start reading the file. When it is done, calls the onload event defined above.
+      reader.readAsBinaryString(fileInput.files[0])
+    }
+    
+    fileInput.addEventListener('change', readFile)
 }
 
 function displayDivSupperssion(){
